124312997a75cd97d1cb50929b23961c1e758cc6
BuildTools <unconfigured@null.spigotmc.org>
2023-01-20 23:41:59 +0900

    ログ機能、VC接続と切断を追加

---
+++ botlog.py
@@ -0,0 +1,38 @@
+# インストールした discord.py を読み込む
+# 自分のBotのアクセストークンに置き換えてください
+# 接続に必要なオブジェクトを生成
+
+discord = None
+client = None
+async def voice_log(CHANNEL_ID, member, before, after):
+    from datetime import datetime
+
+    if member.guild.id == 720532235987451983 and (before.channel != after.channel):
+            now = datetime.now()
+            alert_channel = client.get_channel(CHANNEL_ID)
+            if before.channel is None:
+                msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+                await alert_channel.send(msg)
+            elif after.channel is None:
+                msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+                await alert_channel.send(msg)
+
+async def message_delete_log(CHANNEL_ID,message):
+    from datetime import datetime
+
+    now = datetime.now()
+
+    embed = discord.Embed(title="メッセージ削除", color=discord.Color.red())
+    embed.add_field(name="メッセージ", value=message.content, inline=False)
+    embed.add_field(name="時刻", value=now.strftime('%Y /%m / %d　 %H : %M : %S'), inline=False)
+    embed.add_field(name="チャンネル", value=message.channel.mention, inline=False)
+    embed.set_footer(icon_url=message.author.avatar.url, text=message.author.display_name)
+    channel = message.guild.get_channel(CHANNEL_ID)
+    await channel.send(embed=embed)
+
+
+async def message_edit_log(CHANNEL_ID,before, after):
+    channel = client.get_channel(CHANNEL_ID)
+
+    await channel.send(before.content)
+    await channel.send(after.content)
--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,11 @@
 # インストールした discord.py を読み込む
-import random
 
+from discord import Intents
 import discord
+
+import botlog as log
+import random
+from discord.channel import VoiceChannel
 
 # 自分のBotのアクセストークンに置き換えてください
 TOKEN = ''
@@ -9,28 +13,42 @@ CHANNEL_ID = 720532235987451986
 # 接続に必要なオブジェクトを生成
 intents = discord.Intents.default()
 intents.members = True
+intents.voice_states = True
+intents.messages = True
 client = discord.Client(intents=discord.Intents.all())
+log.client = client
+log.discord = discord
+
+voiceChannel: VoiceChannel
+
+voiceChannel_id = 720532235987451988
+
 
 async def start():
     channel = client.get_channel(CHANNEL_ID)
     await channel.send("ログインしました。")
 
 
-#メンバー取得処理
+# メンバー取得処理。ロールIDは要リクエスト
 async def getMem(command):
     member = command.channel.members
-    memberlist = []
-    for i in member:
-        memberlist.append(i.id)
-    print(memberlist)
-    await command.channel.send('取得しました')
-    return memberlist
 
-#ランダム抽選処理
+    member_list = []
+    for i in member:
+        role = i.roles
+        require_role = discord.utils.find(lambda role: role.id == 1065901716240863292, command.guild.roles)
+        if require_role in role:
+            member_list.append(i.id)
+    print(member_list)
+    await command.channel.send('取得しました')
+    return member_list
+
+
+# ランダム抽選処理
 async def roulette(message1):
-    allmenber = await getMem(message1)
-    choicemenber =  random.choice(allmenber)
-    return choicemenber
+    all_member = await getMem(message1)
+    choice_member = random.choice(all_member)
+    return choice_member
 
 
 # 起動時に動作する処理
@@ -39,6 +57,7 @@ async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
     await start()
+
 
 # メッセージ受信時に動作する処理
 @client.event
@@ -50,11 +69,40 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
-    #ランダム抽選コマンド
+    # ランダム抽選コマンド
     elif message.content == '/roulette':
         log = await roulette(message)
         await message.channel.send(" <@{}> さん当選おめでとうございます！".format(log))
+    elif message.content == '!connect':
+        await VoiceChannel.connect(message.author.voice.channel)
+        await message.channel.send('読み上げBotが参加しました')
 
+
+    elif message.content == '!disconnect':
+        server = message.guild.voice_client
+        await server.disconnect()
+
+        await message.channel.send('読み上げBotが退出しました')
+
+
+
+
+# メッセージVCログイン云々
+@client.event
+async def on_voice_state_update(member, before, after):
+    await log.voice_log(CHANNEL_ID, member, before, after)
+
+
+# メッセージ削除ログ
+@client.event
+async def on_message_delete(message):
+    await log.message_delete_log(CHANNEL_ID, message)
+
+
+# メッセージ編集ログ
+@client.event
+async def on_message_edit(before, after):
+    await log.message_edit_log(CHANNEL_ID, before, after)
 
 
 # Botの起動とDiscordサーバーへの接続
